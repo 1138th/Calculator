6dfc6defbf3154c6058cda864e13330ac9509d95
Ansar.Daku <dakuansar7@gmail.com>
2019-06-23 14:40:43 +0600

    Edited some tests.

--- src/test/java/CalulatorTest.java
+++ src/test/java/CalulatorTest.java
@@ -6,35 +6,48 @@ public class CalulatorTest {
 
     private Calculator calc = new Calculator();
 
-    @Test public void testMult(){
+    @Test
+    public void testMult(){
         Assert.assertEquals(11.2, calc.mult(5.6, 2));
     }
 
-    @Test public void testDiv(){
+    @Test(expectedExceptions = NumberFormatException.class)
+    public void testDiv(){
+        Assert.assertEquals(NumberFormatException.class, calc.div(1, 0));
+    }
+
+    @Test(expectedExceptions = NumberFormatException.class)
+    public void testDivDoubles(){
         Assert.assertEquals(ArithmeticException.class, calc.div(1.0, 0));
     }
 
-    @Test public void testPow(){
+    @Test
+    public void testPow(){
         Assert.assertEquals(Math.sqrt(125), calc.pow(5, 1.5));
     }
 
-    @Test public void testSqrt() {
-        Assert.assertEquals(ArithmeticException.class, calc.sqrt(-5));
+    @Test
+    public void testSqrt() {
+        Assert.assertEquals(Math.sqrt(-5), calc.sqrt(-5));
     }
 
-    @Test public void tesTg(){
+    @Test
+    public void tesTg(){
         Assert.assertEquals( Math.tan(30), calc.tg(30));
     }
 
-    @Test public void testCtg(){
+    @Test
+    public void testCtg(){
         Assert.assertEquals(1/Math.tan(30), calc.ctg(30));
     }
 
-    @Test public void testCos(){
+    @Test
+    public void testCos(){
         Assert.assertEquals(Math.cos(30), calc.cos(30));
     }
 
-    @Test public void testSin(){
+    @Test
+    public void testSin(){
         Assert.assertEquals(Math.sin(30), calc.sin(30));
     }
 }
