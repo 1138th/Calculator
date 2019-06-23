3053237cf093de61f125df4990bcdbfeba645c66
Ansar.Daku <dakuansar7@gmail.com>
2019-06-23 13:57:52 +0600

    Initiate commit. Added library calculator. Added test library. Created 8 tests.

---
+++ src/test/java/CalulatorTest.java
@@ -0,0 +1,40 @@
+import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalulatorTest {
+
+    private Calculator calc = new Calculator();
+
+    @Test public void testMult(){
+        Assert.assertEquals(11.2, calc.mult(5.6, 2));
+    }
+
+    @Test public void testDiv(){
+        Assert.assertEquals(ArithmeticException.class, calc.div(1.0, 0));
+    }
+
+    @Test public void testPow(){
+        Assert.assertEquals(Math.sqrt(125), calc.pow(5, 1.5));
+    }
+
+    @Test public void testSqrt() {
+        Assert.assertEquals(ArithmeticException.class, calc.sqrt(-5));
+    }
+
+    @Test public void tesTg(){
+        Assert.assertEquals( Math.tan(30), calc.tg(30));
+    }
+
+    @Test public void testCtg(){
+        Assert.assertEquals(1/Math.tan(30), calc.ctg(30));
+    }
+
+    @Test public void testCos(){
+        Assert.assertEquals(Math.cos(30), calc.cos(30));
+    }
+
+    @Test public void testSin(){
+        Assert.assertEquals(Math.sin(30), calc.sin(30));
+    }
+}
